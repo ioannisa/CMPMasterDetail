60e322f10a62b437654f571db3a5cf02a5443904
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-03-16 18:12:28 +0200

    Removed default app's platform implementation

--- composeApp/src/commonMain/kotlin/eu/anifantakis/cmpmasterdetail/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package eu.anifantakis.cmpmasterdetail
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
