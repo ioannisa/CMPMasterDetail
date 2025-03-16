44969627ce66cc9efaa16c3f30ed2a7bf87344ae
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-03-16 18:25:04 +0200

    Fix: Typo

--- composeApp/src/commonMain/kotlin/eu/anifantakis/cmpmasterdetail/movies/data/datasource/RemoteMoviesDataSource.kt
+++ composeApp/src/commonMain/kotlin/eu/anifantakis/cmpmasterdetail/movies/data/datasource/RemoteMoviesDataSource.kt
@@ -5,7 +5,7 @@ import eu.anifantakis.cmpmasterdetail.core.domain.util.DataResult
 import eu.anifantakis.cmpmasterdetail.core.domain.util.map
 import eu.anifantakis.cmpmasterdetail.movies.data.mappers.toMovie
 import eu.anifantakis.cmpmasterdetail.movies.data.networking.MoviesHttpClient
-import eu.anifantakis.cmpmasterdetail.movies.data.networking.dto.MoviesDtp
+import eu.anifantakis.cmpmasterdetail.movies.data.networking.dto.MoviesDto
 import eu.anifantakis.cmpmasterdetail.movies.domain.Movie
 import eu.anifantakis.cmpmasterdetail.movies.domain.datasource.RemoteMoviesDataSource
 
@@ -13,7 +13,7 @@ class RemoteMoviesDataSourceImpl(
     private val httpClient: MoviesHttpClient
 ) : RemoteMoviesDataSource {
     override suspend fun getMovies(): DataResult<List<Movie>, DataError.Network> {
-        return httpClient.get<MoviesDtp>(
+        return httpClient.get<MoviesDto>(
             route = "/3/movie/top_rated"
         ).map { moviesDto ->
             moviesDto.results.map { movieDto ->
